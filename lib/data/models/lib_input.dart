3502484f7ac882a7aa3f6b67ddc14aab67270bdf
Armel Cadet-Petit <arm-cade@proton.me>
2024-11-24 14:58:52 +0100

    Fix `get_seats` and `get_inputs` commands on some platforms ... again

--- lib/data/models/lib_input.dart
+++ lib/data/models/lib_input.dart
@@ -38,73 +38,91 @@ class LibInput extends Equatable {
   /// Whether events are being sent by the device. It can be enabled, disabled,
   /// or disabled_on_external_mouse
   @JsonKey(name: 'send_events')
-  final String sendEvents;
+  final String? sendEvents;
 
   /// Whether tap to click is enabled. It can be enabled or disabled
-  final String tap;
+  final String? tap;
 
   /// The finger to button mapping in use. It can be lmr or lrm
   @JsonKey(name: 'tap_button_map')
-  final String tapButtonMap;
+  final String? tapButtonMap;
 
   /// Whether tap-and-drag is enabled. It can be enabled or disabled
   @JsonKey(name: 'tap_drag')
-  final String tapDrag;
+  final String? tapDrag;
 
   /// Whether drag-lock is enabled. It can be enabled or disabled
   @JsonKey(name: 'tap_drag_lock')
-  final String tapDragLock;
+  final String? tapDragLock;
 
   /// The pointer-acceleration in use
   @JsonKey(name: 'accel_speed')
-  final double accelSpeed;
+  final double? accelSpeed;
 
   /// The acceleration profile in use. It can be none, flat, or adaptive
   @JsonKey(name: 'accel_profile')
-  final String accelProfile;
+  final String? accelProfile;
 
   /// Whether natural scrolling is enabled. It can be enabled or disabled
   @JsonKey(name: 'natural_scroll')
-  final String naturalScroll;
+  final String? naturalScroll;
 
   /// Whether left-handed mode is enabled. It can be enabled or disabled
   @JsonKey(name: 'left_handed')
-  final String leftHanded;
+  final String? leftHanded;
 
   /// The click method in use. It can be none, button_areas, or clickfinger
   @JsonKey(name: 'click_method')
-  final String clickMethod;
+  final String? clickMethod;
 
   /// Whether middle emulation is enabled. It can be enabled or disabled
   @JsonKey(name: 'middle_emulation')
-  final String middleEmulation;
+  final String? middleEmulation;
 
   /// The scroll method in use. It can be none, two_finger, edge,
   /// or on_button_down
   @JsonKey(name: 'scroll_method')
-  final String scrollMethod;
+  final String? scrollMethod;
 
   /// The scroll button to use when scroll_method is on_button_down.
   /// This will be given as an input event code
   @JsonKey(name: 'scroll_button')
-  final int scrollButton;
+  final int? scrollButton;
 
   /// Whether scroll button lock is enabled. It can be enabled or disabled
   @JsonKey(name: 'scroll_button_lock')
-  final String scrollButtonLock;
+  final String? scrollButtonLock;
 
   /// Whether disable-while-typing is enabled. It can be enabled or disabled
-  final String dwt;
+  final String? dwt;
 
   /// Whether disable-while-trackpointing is enabled.
   /// It can be enabled or disabled
-  final String dwtp;
+  final String? dwtp;
 
   /// An array of 6 floats representing the calibration matrix for absolute
   /// devices such as touchscreens
   @JsonKey(name: 'calibration_matrix')
-  final List<double> calibrationMatrix;
+  final List<double>? calibrationMatrix;
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [
+        sendEvents,
+        tap,
+        tapButtonMap,
+        tapDrag,
+        tapDragLock,
+        accelSpeed,
+        accelProfile,
+        naturalScroll,
+        leftHanded,
+        clickMethod,
+        middleEmulation,
+        scrollMethod,
+        scrollButton,
+        scrollButtonLock,
+        dwt,
+        dwtp,
+        calibrationMatrix,
+      ];
 }
